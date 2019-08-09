904b90d71e13e09f275656fc60e7cb2fe9a38b84
Ellen2018 <1961075191@qq.com>
2019-08-09 18:36:25 +0800

    更新选择排序和插入排序

--- src/Sort.java
+++ src/Sort.java
@@ -8,7 +8,7 @@ public class Sort {
         int times = 0;
         boolean isExchage = false;
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length - (i+1); j++) {
+            for (int j = 0; j < a.length - (i + 1); j++) {
                 times++;
                 if (a[j] > a[j + 1]) {
                     int zhong = a[j];
@@ -18,11 +18,67 @@ public class Sort {
                 }
             }
 
-            if(!isExchage){
+            if (!isExchage) {
                 break;
             }
         }
         System.out.println("比较的次数：" + times);
+    }
+
+    //选择排序
+    public static void xuanZhe(int[] a) {
+        if (isOrdered(a)) {
+            return;
+        }
+        int times = 0;
+        for (int i = 0; i < a.length; i++) {
+            int currentIndex = i;
+            for (int j = i + 1; j < a.length; j++) {
+                times++;
+                if (a[j] < a[currentIndex]) {
+                    currentIndex = j;
+                }
+            }
+            //进行交换
+            if (currentIndex != i) {
+                int zhong = a[currentIndex];
+                a[currentIndex] = a[i];
+                a[i] = zhong;
+            }
+        }
+        System.out.println("比较的次数:" + times);
+    }
+
+    //插入排序
+    public static void chaRu(int[] a){
+        int times = 0;
+        for(int i=0;i<a.length;i++){
+            for(int j=i;j>0;j--){
+                times++;
+               if(a[j] < a[j-1]){
+                   int zhong = a[j];
+                   a[j] = a[j-1];
+                   a[j-1] = zhong;
+               }
+            }
+        }
+        System.out.println("比较的次数："+times);
+    }
+
+    public static boolean isOrdered(int[] a) {
+        int orderCount = 0;
+        int times = 0;
+        for (int i = 0; i < a.length - 1; i++) {
+            times++;
+            if (a[i + 1] >= a[i]) {
+                orderCount++;
+            }
+        }
+        if (orderCount == a.length - 1) {
+            System.out.println("比较的次数：" + times);
+            return true;
+        }
+        return false;
     }
 
     public static void printfArray(int[] a) {
@@ -32,8 +88,8 @@ public class Sort {
     }
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8};
-        maoPao(a);
+        int[] a = {1, 2, 3, 4, 5, 6, 7, 8};
+        chaRu(a);
         printfArray(a);
     }
 
